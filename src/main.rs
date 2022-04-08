afebe3fbc880081fb80d520bf1fa186cf648fe69
Growthack90 <hackmaker90@outlook.it>
2022-04-08 11:40:53 +0200

    add cli.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+
+pub enum Action {
+    Add { task: String },
+    Done { position: usize },
+    List,
+}
+
+pub struct CommandLineArgs {
+    pub action: Action,
+    pub journal_file: Option<PathBuf>,
+}
+
+mod cli;
 fn main() {
     println!("Hello, world!");
 }
