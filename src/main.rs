a972d1e4c2e9b2b3a4bd74cca0d602bea408f813
Growthack90 <hackmaker90@outlook.it>
2022-04-08 14:52:35 +0200

    set file journal

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,25 @@ mod tasks;
 
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
+use std::path::PathBuf;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
 
 fn main() {
+
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file.");
     // Get the command-line arguments.
     let CommandLineArgs {
         action,
