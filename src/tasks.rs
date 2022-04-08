c6ea3d77aec248e2fe1991fce876405547a115e8
Growthack90 <hackmaker90@outlook.it>
2022-04-08 12:29:38 +0200

    interaction with file system

--- src/tasks.rs
+++ src/tasks.rs
@@ -2,6 +2,9 @@ use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
 
+use std::io::Result;
+use std::path::PathBuf;
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
@@ -9,3 +12,9 @@ pub struct Task {
     #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> { ... }
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> { ... }
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> { ... }
